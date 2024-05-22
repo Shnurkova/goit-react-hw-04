83ae3de4f9a0e33de7119e459b4611b025a335d9
Shnurkova <Annshnur@gmail.com>
2024-05-22 18:24:43 +0300

    fix

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,20 +2,18 @@ import { Audio } from 'react-loader-spinner';
 
 export default function Loader(){
 
-    return(
+    return (
         <div>
-        
-        <Audio
-  height="80"
-  width="80"
-  radius="9"
-  color="blue"
-  ariaLabel="loading"
-  wrapperStyle
-  wrapperClass
-/>
-<p>Loading data, please wait...</p>
+            <Audio
+                height="80"
+                width="80"
+                radius="9"
+                color="blue"
+                ariaLabel="loading"
+                wrapperStyle
+                wrapperClass
+            />
+            <p>Loading data, please wait...</p>
         </div>
-
-    )
+    );
 }
